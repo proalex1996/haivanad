a0af8fe33c91b1d0168bc013c0028006815613fd
proalex1996 <trantrungtinh11a2@gmail.com>
2021-04-27 09:22:30 +0700

    ádads

--- resources/views/pages/top-page/master.blade.php
+++ resources/views/pages/top-page/master.blade.php
@@ -223,14 +223,6 @@ header('Access-Control-Allow-Origin: *');
 
             </div>
         </div>
-        {{--        <div class="col-md-12">--}}
-        {{--            <ul class="social-network social-circle">--}}
-        {{--                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>--}}
-        {{--                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>--}}
-        {{--                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>--}}
-        {{--                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>--}}
-        {{--            </ul>--}}
-        {{--        </div>--}}
         <a href="https://zalo.me/" id="linkzalo" target="_blank" rel="noopener noreferrer">
             <div class="fcta-zalo-vi-tri-nut">
                 <div id="fcta-zalo-tracking" class="fcta-zalo-nen-nut">
